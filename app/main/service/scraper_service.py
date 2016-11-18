229c3a05170c27460df08b8fffadaf266a6ca116
clementlefevre <clement.san@gmai.com>
2016-11-18 17:31:35 +0100

    add

--- app/main/service/scraper_service.py
+++ app/main/service/scraper_service.py
@@ -40,6 +40,8 @@ def retrieve_description(agent):
     except urllib2.HTTPError as e:
 
         print " HTTPError by parsing : {}".format(agent.keywords.encode('utf-8'))
+        print "{0} : Error :".format(proxy_data['ip'] + ":" + proxy_data['type'] + ":" + proxy_data[
+            'level'] + ":" + proxy_data['country'])
         return []
 
     except Exception as e:
